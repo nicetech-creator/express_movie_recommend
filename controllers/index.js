67015a81d3d597fb2790f14b0672a16ca7c811a4
nicetech-creator <toptech20190609@gmail.com>
2022-10-26 21:51:22 -0400

    getRecommendations: Find out favorite director & genre from rated moives list

--- controllers/index.js
+++ controllers/index.js
@@ -14,7 +14,41 @@ async function getRecommendations(req, res) {
   } catch (error) {
     res.status(500).send(error.toString())
   }
-  res.send(ratedMovies)
+
+  // Figure out favorite director, favorite genre
+  const RateThreashold = 7
+  let favDirector = null
+  let favGenre = null
+  let directorRatings = {}
+  let genreRatings = {}
+  let maxDirectorRate = 0
+  let maxGenreRate = 0
+
+  ratedMovies.forEach((movie) => {
+    if (movie.userRating >= RateThreashold) {
+      if (directorRatings.hasOwnProperty(movie.director)) 
+        directorRatings[movie.director] += 1
+      else 
+        directorRatings[movie.director] = 1
+      if (directorRatings[movie.director] > maxDirectorRate) {
+        maxDirectorRate = directorRatings[movie.director]
+        favDirector = movie.director
+      }
+
+      movie.genres.forEach((gen) => {
+        if (genreRatings.hasOwnProperty(gen)) 
+          genreRatings[gen] += 1
+        else 
+          genreRatings[gen] = 1
+        if (genreRatings[gen] > maxGenreRate) {
+          maxGenreRate = genreRatings[gen]
+          favGenre = gen
+        }
+      })
+    }
+  })
+
+  res.status(200).send({favDirector, favGenre})
 }
 
 module.exports = {
