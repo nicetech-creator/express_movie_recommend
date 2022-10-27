b03016149019d3d39a5739e853406204ba983520
nicetech-creator <toptech20190609@gmail.com>
2022-10-26 21:16:29 -0400

    Express API structure, basic router

--- app.js
+++ app.js
@@ -8,11 +8,14 @@ const {
   getSavedRecommendations,
 } = require('./helperFunctions')
 
+const router = require('./routes')
+
 const LISTENING_PORT = 9000
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use('/', router)
 
 const server = app.listen(LISTENING_PORT, function () {
   console.log(`Server is listening on ${LISTENING_PORT}`)
